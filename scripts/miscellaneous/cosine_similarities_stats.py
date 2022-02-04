fixing bug on the computation of dirichlet

scripts/miscellaneous/cosine_similarities_stats.py
@@ -57,7 +57,12 @@ def dirichlet_generator(signature, cosines_df, n_draws, n_channels, alpha=1):
                 pval = sum((np.array(cosines) >= cosines_df[c][el])) / n_draws
                 logpval = [-np.log10(pval) if pval > 0 else -np.log10(1/n_draws)][0]
                 df[c][el] = logpval
+                if c == 'Tombo MMS' and el == 'Sancar Cisplatin':
+                    import pdb;pdb.set_trace()
 
+                if c == 'Sancar Cisplatin' and el == 'Tombo MMS':
+                    import pdb;pdb.set_trace()
+    import pdb;pdb.set_trace()
     return df
 
 def dirichlet_generator_mao(signature, cosines_df, df, n_draws, n_channels, alpha=1):
@@ -72,6 +77,34 @@ def dirichlet_generator_mao(signature, cosines_df, df, n_draws, n_channels, alph
         logpval = [-np.log10(pval) if pval > 0 else -np.log10(1/n_draws)][0]
         df['Mao MMS'][el] = logpval
         df[el]['Mao MMS'] = logpval
+                
+    return df
+
+
+def dirichlet_generator_mao_2(signature, cosines_df, df, n_draws, n_channels, alpha=1):
+
+
+    signatures = signature.columns
+
+    for c in tqdm(signatures):
+        sig = signature[c].values
+        s_pool = np.random.dirichlet(
+            alpha=[alpha for _ in range(n_channels)], size=n_draws
+        )
+        cosines = list(map(lambda x: 1-cosine(x, sig), s_pool))
+        for el in cosines_df.index:
+            if c == 'Mao MMS':
+                pval = sum((np.array(cosines) >= cosines_df[c][el])) / n_draws
+                logpval = [-np.log10(pval) if pval > 0 else -np.log10(1/n_draws)][0]
+                df[c][el] = logpval
+                
+            else:
+                if el == 'Mao MMS':
+                    pval = sum((np.array(cosines) >= cosines_df['Mao MMS'][el])) / n_draws
+                    logpval = [-np.log10(pval) if pval > 0 else -np.log10(1/n_draws)][0]
+                    df[c][el] = logpval
+
+    import pdb;pdb.set_trace()
                 
     return df
 
@@ -152,16 +185,25 @@ def do_plots(labels, logpvals, means, alpha, label, output):
     plt.close()
 
 
-def plot_heatmap(df, output):
+def plot_heatmap(df, df_cos, output):
 
-    mask = np.zeros_like(df.values)
+    mask = np.zeros_like(df_cos.values)
     mask[np.triu_indices_from(mask)] = True
-
+    
     with sns.axes_style("white"):
         fig, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(df, mask=mask, square=True, cmap='Blues', 
-            cbar_kws={'label': '-   log10 p-value'}, annot=True)
+        ax = sns.heatmap(df_cos, mask=mask, square=True, cmap='Blues', 
+            cbar_kws={'label': 'Cosine Similarity'}, annot=True)
     
+    
+    from matplotlib.patches import Rectangle
+    for i in range(df_cos.shape[0]):
+        for k in range(df_cos.shape[1]):
+            if (df.iloc[i][k] >= 2) and (mask[i,k] == 0):
+                ax.add_patch(Rectangle(
+                    (k, i), 1, 1, fill=False, edgecolor='black', lw=2)
+                )
+
     outdir = os.path.join(output, "heatmap_stats_cosines.pdf")
     fig.tight_layout()
 
@@ -192,15 +234,15 @@ def main(signatures_all, signatures_mao, data_cosines, alpha, n_samples, output)
     # )
     # do_plots(labels_mao, logpvals_mao, means_mao, alpha, 'mao', output)
 
-    test_alphas(64, 'all', output)
-    test_alphas(32, 'mao', output)
+    # test_alphas(64, 'all', output)
+    # test_alphas(32, 'mao', output)
 
     df_pvals = dirichlet_generator(sig, df, n_samples, 64, alpha=alpha)
-    df_all = dirichlet_generator_mao(
+    df_all = dirichlet_generator_mao_2(
         sig_mao, df, df_pvals, n_samples, 32, alpha=alpha
     )
-
-    plot_heatmap(df_all, output)
+    import pdb;pdb.set_trace()
+    plot_heatmap(df_all, df, output)
 
 
 if __name__ == '__main__':